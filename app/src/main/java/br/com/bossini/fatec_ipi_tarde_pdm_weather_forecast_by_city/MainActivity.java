70cc478720d2ef7a8ce3e1ed1733cf828ec5b438
Rodrigo Bossini <professorbossini@gmail.com>
2019-09-20 16:11:15 -0300

    Fizemos a requisição usando new Thread. JSON precisa ser manipulado ainda.

--- app/src/main/java/br/com/bossini/fatec_ipi_tarde_pdm_weather_forecast_by_city/MainActivity.java
+++ app/src/main/java/br/com/bossini/fatec_ipi_tarde_pdm_weather_forecast_by_city/MainActivity.java
@@ -8,16 +8,46 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.NetworkOnMainThreadException;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView weatherListView;
+    private List<Weather> weatherList;
+
+    private EditText locationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        weatherListView = findViewById(R.id.weatherListView);
+        locationEditText = findViewById(R.id.locationEditText);
+        weatherList = new ArrayList<>();
+        ArrayAdapter <Weather> adapter =
+                new ArrayAdapter<Weather>(
+                        this,
+                        android.R.layout.simple_list_item_1,
+                        weatherList
+                );
+        weatherListView.setAdapter(adapter);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -25,8 +55,63 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                String cidade =
+                        locationEditText.
+                                getEditableText().toString();
+                String endereco =
+                        getString(
+                                R.string.web_service_url,
+                                cidade,
+                                getString(
+                                        R.string.api_key
+                                )
+                        );
+                /*Toast.makeText(
+                        MainActivity.this,
+                        endereco,
+                        Toast.LENGTH_SHORT
+                ).show();*/
+                new Thread(
+                        () -> {
+                            try{
+                                URL url = new URL (endereco);
+                                HttpURLConnection conn =
+                                        (HttpURLConnection) url.openConnection();
+                                InputStream is = conn.getInputStream();
+                                InputStreamReader isr =
+                                        new InputStreamReader (is);
+                                BufferedReader reader =
+                                        new BufferedReader(isr);
+                                //reader.readLine();
+                                String linha = null;
+                                StringBuilder s = new StringBuilder("");
+                                while ((linha = reader.readLine()) != null){
+                                    s.append(linha);
+                                }
+                                reader.close();
+                                //locationEditText.getText();
+                                runOnUiThread(()->{
+                                    Toast.makeText(
+                                            MainActivity.this,
+                                            "",
+                                            Toast.LENGTH_LONG
+                                    ).show();
+                                });
+
+                            }
+                            catch (IOException e){
+                                e.printStackTrace();
+                                Toast.makeText(
+                                        MainActivity.this,
+                                        getString(R.string.connect_error),
+                                        Toast.LENGTH_SHORT
+                                ).show();
+                            }
+
+                        }
+                )
+                .start();
+
             }
         });
     }
---
+++ app/src/main/java/br/com/bossini/fatec_ipi_tarde_pdm_weather_forecast_by_city/Weather.java
@@ -0,0 +1,56 @@
+package br.com.bossini.fatec_ipi_tarde_pdm_weather_forecast_by_city;
+
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class Weather {
+    public final String dayOfWeek;
+    public final String description;
+    public final String minTemp;
+    public final String maxTemp;
+    public final String humidity;
+    public final String iconURL;
+
+    public Weather (
+            long timeStamp,
+            double minTemp,
+            double maxTemp,
+            double humidity,
+            String description,
+            String iconName
+    ){
+        this.dayOfWeek =
+                convertTimestampToDay (timeStamp);
+        NumberFormat numberFormat =
+                NumberFormat.getInstance();
+        numberFormat.setMaximumFractionDigits(0);
+        this.minTemp = numberFormat.format(minTemp) + "\u00B0C";
+        this.maxTemp = numberFormat.format(maxTemp) + "\u00B0C";
+        this.humidity =
+                NumberFormat.
+                        getPercentInstance().format(
+                                humidity / 100.0);
+        this.description = description;
+        this.iconURL = String.format(
+                Locale.getDefault(),
+                "http://openweathermap.org/img/w/%s.png",
+                iconName
+        );
+    }
+    public String convertTimestampToDay (long timeStamp){
+        Calendar agora = Calendar.getInstance();
+        agora.setTimeInMillis( timeStamp * 1000);
+        TimeZone fusoHorario = TimeZone.getDefault();
+        agora.add(Calendar.MILLISECOND,
+                fusoHorario.getOffset(
+                        agora.getTimeInMillis()
+                ));
+        return new SimpleDateFormat("EEEE").format(
+                agora.getTime()
+        );
+
+    }
+}
